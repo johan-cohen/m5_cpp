b06448ab0a22afd60454557a7ecc115d2e167e69
Flavio Santes <flavio.santes@1byt3.com>
2017-10-12 14:56:19 -0400

    Common: Add some common MQTT 5 constants and algorithms
    - Add some MQTT 5 constants.
    - Add the PktQoS and PktType enums.
    - Add an algorithm to compute the wire size of a given uint32_t according
    to the Variable Byte Integer algorithm.
    
    Signed-off-by: Flavio Santes <flavio.santes@1byt3.com>

---
+++ src/Common.hpp
@@ -0,0 +1,98 @@
+/*
+ *                                   1byt3
+ *
+ *                              License Notice
+ *
+ * 1byt3 provides a commercial license agreement for this software. This
+ * commercial license can be used for development of proprietary/commercial
+ * software. Under this commercial license you do not need to comply with the
+ * terms of the GNU Affero General Public License, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * If you don't receive a commercial license from us (1byt3), you MUST assume
+ * that this software is distributed under the GNU Affero General Public
+ * License, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Contact us for additional information: customers at 1byt3.com
+ *
+ *                          End of License Notice
+ */
+
+/*
+ * MQTT 5 Low Level Packet Library
+ *
+ * Copyright (C) 2017 1byt3, customers at 1byt3.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdint.h>
+
+namespace m5 {
+
+static const uint32_t binaryLenSize = 2;
+static const uint32_t stringLenSize = binaryLenSize;
+static const uint32_t integerLenSize = 2;
+
+static const char protocolStr[] = "MQTT";
+static const uint32_t protocolVersion5 = 0x05;
+
+static const uint32_t clientIdMinLen = 1;
+static const uint32_t clientIdMaxLen = 23;
+
+enum class PktQoS {
+	QoS0 = 0,
+	QoS1,
+	QoS2
+};
+
+enum class PktType {
+	RESERVED = 0,
+	CONNECT,
+	CONNACK,
+	PUBLISH,
+	PUBACK,
+	PUBREC,
+	PUBREL,
+	PUBCOMP,
+	SUBSCRIBE,
+	SUBACK,
+	UNSUBSCRIBE,
+	UNSUBACK,
+	PINGREQ,
+	PINGRESP,
+	DISCONNECT,
+	AUTH
+};
+
+static inline int VBIWireSize(uint32_t value)
+{
+	if (value <= 127) {
+		return 1;
+	}
+	if (value <= 16383) {
+		return 2;
+	}
+	if (value <= 2097151) {
+		return 3;
+	}
+	if (value <= 268435455) {
+		return 4;
+	}
+
+	return 0;
+}
+
+}
