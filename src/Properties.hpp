19dfe0e2e1e6af6cd605a4a353a3c8cbfeb946ad
Flavio Santes <flavio.santes@1byt3.com>
2017-10-23 21:03:11 -0400

    Properties: Add routines for 'Server Reference'
    Signed-off-by: Flavio Santes <flavio.santes@1byt3.com>

--- src/Properties.hpp
+++ src/Properties.hpp
@@ -171,6 +171,10 @@ public:
 	void responseInformation(const char *str);
 	const uint8_t *responseInformation(uint16_t &size);
 
+	void serverReference(const uint8_t *data, uint16_t size);
+	void serverReference(const char *str);
+	const uint8_t *serverReference(uint16_t &size);
+
 	void receiveMaximum(uint16_t v);
 	uint16_t receiveMaximum(void);
 
