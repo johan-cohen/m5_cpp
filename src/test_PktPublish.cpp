43684db58759a467ab569e2047d23d73fe054aa0
Flavio Santes <flavio.santes@1byt3.com>
2017-11-03 11:05:18 -0400

    PktPublish: Add the test case
    Signed-off-by: Flavio Santes <flavio.santes@1byt3.com>

--- src/test_PktPublish.cpp
+++ src/test_PktPublish.cpp
@@ -41,17 +41,80 @@
 #include "PktPublish.hpp"
 #include "test_Common.hpp"
 
+#include <cstring>
+
 int test(void)
 {
 	m5::PktPublish *publish;
+	const char str[] = "Hello, World!";
+	const m5::ByteArray array(str, str + strlen(str));
+	const uint8_t *payload = (const uint8_t *)str;
+	const uint16_t payloadSize = strlen(str);
+	uint16_t u16 = 0xABCD;
+	m5::AppBuf buf(128);
 
 	publish = new m5::PktPublish();
+
+	publish->retain(true);
+	if (publish->retain() != true) {
+		throw std::logic_error("Retain");
+	}
+
+	publish->QoS(m5::PktQoS::QoS2);
+	if (publish->QoS() != m5::PktQoS::QoS2) {
+		throw std::logic_error("QoS");
+	}
+
+	publish->dup(true);
+	if (publish->dup() != true) {
+		throw std::logic_error("DUP");
+	}
+
+	publish->topic(str);
+	if (publish->topic() != array) {
+		throw std::logic_error("Topic");
+	}
+
+	publish->packetId(u16);
+	if (publish->packetId() != u16) {
+		throw std::logic_error("Packet Id");
+	}
+
+	publish->payload(payload, payloadSize);
+	if (publish->payload() != array) {
+		throw std::logic_error("Payload");
+	}
+
+	publish->writeTo(buf);
+
+	m5::PktPublish publishRead;
+	publishRead.readFrom(buf);
+
+	if (publish->retain() != publishRead.retain()) {
+		throw std::logic_error("read: Retain");
+	}
+	if (publish->QoS() != publishRead.QoS()) {
+		throw std::logic_error("read: QoS");
+	}
+	if (publish->dup() != publishRead.dup()) {
+		throw std::logic_error("read: DUP");
+	}
+	if (publish->topic() != publishRead.topic()) {
+		throw std::logic_error("read: Topic");
+	}
+	if (publish->packetId() != publishRead.packetId()) {
+		throw std::logic_error("read: Packet Id");
+	}
+	if (publish->payload() != publishRead.payload()) {
+		throw std::logic_error("read: Payload");
+	}
+
 	delete publish;
 
 	return 0;
 }
 
-int main()
+int main(void)
 {
 	int rc;
 
